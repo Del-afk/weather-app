2f7eff5e2e1ef14df83861a5972850acb089d5a3
del_afk <aramafkhamird@gmail.com>
2022-06-11 00:42:02 +0430

    i hate loops

--- index.js
+++ index.js
@@ -108,6 +108,46 @@ function callApi(city) {
         windSpeed(data.wind.speed);
         humidity(data.main.humidity);
         changeImage(data.weather[0].icon);
+
+        // let lan = data.lan;
+        // let lon = data.lon;
+        // let forcastUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&appid=${weatherApiKey}`;
+        // fetch(forcastUrl).then().then()
+        forcastApi(data.coord.lat, data.coord.lon);
+      }
+    });
+}
+
+function forcastApi(lat, lon) {
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude={current,minutely,hourly,alerts}&appid=${weatherApiKey}&units=metric`;
+
+  fetch(apiUrl)
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      if (data.cod == "404") {
+        displayCity("this city doesn't exist!");
+      } else {
+        console.log(data.daily[0]);
+
+        for (let i = 0; i < 2; i++) {
+          let forcastDegree = document.getElementById(`degree_d${i}`);
+          forcastDegree.innerHTML = Math.round(data.daily[i].temp.day);
+
+          let forcastHumidity = document.getElementById(`humidity_d${i}`);
+          forcastHumidity.innerHTML = `H: ${Math.round(
+            data.daily[i].humidity
+          )}%`;
+
+          let forcastWindSpeed = document.getElementById(`windspeed_d${i}`);
+          forcastWindSpeed.innerHTML = `WS : ${Math.round(
+            data.daily[i].wind_speed
+          )}Km/h`;
+
+          let forcaststatus = document.getElementById(`status_d${i}`);
+          forcaststatus.innerHTML = data.daily[i].weather[0].main;
+        }
       }
     });
 }
